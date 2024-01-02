ef8d534509bc5f8c8478336399bc12b8065fe1a9
nadaeezzdin <nadaeezzdin@gmail.com>
2024-01-02 20:34:52 +0200

    refactoring

--- api/src/app.js
+++ api/src/app.js
@@ -1,26 +1,23 @@
-
-const express = require('express');
-const admin = require('firebase-admin');
-const { initializedApp } = require('./config/firebase');
-const redditRoutes = require('./routes/redditRoutes');
-
+const express = require("express");
+const cors = require("cors");
+const { initializedApp } = require("./config/firebase");
+const redditRoutes = require("./routes/redditRoutes");
 
 const app = express();
 const port = 5000;
 
 app.use(
   cors({
-    origin: 'http://localhost:3000', 
-    methods: 'GET', 
-    credentials: true, 
+    origin: "http://localhost:3000",
+    methods: "GET",
+    credentials: true,
   })
 );
 
 app.use(express.json());
 
-app.use('/r/flutterDev', redditRoutes);
+app.use("/r/flutterDev", redditRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
